842bd5d5c6ba5101efa5e2c4bf2c6a9cf3c750de
Brendan Bates <batesbrendan@gmail.com>
2019-12-09 00:08:49 +1100

    With day 02 part two solution

--- src/day02/core.clj
+++ src/day02/core.clj
@@ -22,16 +22,26 @@
 
 (assert (= (execute [1, 0, 0, 0, 99]) [2 0 0 0 99]))
 (assert (= (execute [2, 3, 0, 3, 99]) [2 3 0 6 99]))
-(assert (= (execute [2, 4, 4, 5, 99, 0]) [2,4,4,5,99,9801]))
-(assert (= (execute [1,1,1,4,99,5,6,0,99]) [30,1,1,4,2,5,6,0,99]))
+(assert (= (execute [2, 4, 4, 5, 99, 0]) [2, 4, 4, 5, 99, 9801]))
+(assert (= (execute [1, 1, 1, 4, 99, 5, 6, 0, 99]) [30, 1, 1, 4, 2, 5, 6, 0, 99]))
+
+(defn- program-with-noun-and-verb
+  [program noun verb]
+  (-> program (assoc 1 noun) (assoc 2 verb)))
 
 (defn solve-part-one [program]
-  (first (execute (-> program (assoc 1 12) (assoc 2 2)))))
+  (first (execute (program-with-noun-and-verb program 12 2))))
+
+(defn solve-part-two [program]
+  (let [search-for 19690720
+        cs (for [n (range 0 100) v (range 0 100)] [n v])]
+    (let [[n v] (first (filter #(= search-for (first (execute (apply program-with-noun-and-verb program %))))
+                         cs))]
+      (+ (* 100 n) v))))
 
 (defn -main []
   (let [program (as-> (slurp "src/day02/input.txt") $
                       (clojure.string/split $ #",")
-                      (mapv #(Integer/parseInt %) $))]
-    (println "Part 1 >>> " (solve-part-one program))))
-
-(-main)
+                      (mapv (comp #(Integer/parseInt %) clojure.string/trim) $))]
+    (println "Part 1 >>> " (solve-part-one program))
+    (println "Part 2 >>> " (solve-part-two program))))
