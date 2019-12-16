843bd458cc1bac2d6c1e0e99b944941523d6d7df
Brendan Bates <batesbrendan@gmail.com>
2019-12-17 00:07:43 +1100

    With day 8 part 2 solution

--- src/day08/core.clj
+++ src/day08/core.clj
@@ -16,10 +16,33 @@
   (partition-all (* width height)
                  (map #(Integer/parseInt (str %)) in)))
 
+(def pixels {0 :black 1 :white 2 :transparent})
+
+(defn layerise-pixels
+  [stacked-pixels]
+  (or (first (drop-while #(= % 2) stacked-pixels))
+      2))
+
+(assert (= 0 (layerise-pixels [0 1 2 0])))
+(assert (= 1 (layerise-pixels [2 1 2 0])))
+(assert (= 1 (layerise-pixels [2 2 1 0])))
+(assert (= 0 (layerise-pixels [2 2 2 0])))
+(assert (= 2 (layerise-pixels [2 2 2 2])))
+
+(defn solve-part-two
+  [layers]
+  (let [pixels (->> layers
+                    (apply interleave)
+                    (partition-all (count layers))
+                    (map layerise-pixels)
+                    (partition-all 25))]
+    (doseq [row pixels]
+      (println row))))
+
 (defn -main []
   (let [image-layers (-> (slurp "src/day08/input.txt")
                          (clojure.string/trim)
                          (parse-layers [25 6]))]
-    (println "Part 1>>>>" (solve-part-one image-layers))))
+    (println "Part 1>>>>" (solve-part-one image-layers))
+    (solve-part-two image-layers)))
 
-(-main)
