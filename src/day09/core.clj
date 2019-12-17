5b32e5d4c5c82dd20aa8231059e446dfb21cd716
Brendan Bates <batesbrendan@gmail.com>
2019-12-17 22:53:59 +1100

    With day 9 part 2 solution

--- src/day09/core.clj
+++ src/day09/core.clj
@@ -8,9 +8,13 @@
     (intcode/execute program in out)
     @out))
 
+(defn solve-part-two [program]
+  (let [in (atom [2])
+        out (atom [])]
+    (intcode/execute program in out)
+    @out))
+
 (defn -main []
   (let [program (intcode/parse-intcode-program-file "src/day09/input.txt")]
     (println "Part 1 >>> " (solve-part-one program))
-    ))
-
-(-main )
+    (println "Part 2 >>> " (solve-part-two program))))
